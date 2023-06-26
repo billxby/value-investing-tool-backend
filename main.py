f40ad353e71115cd7e9126eec88ce61c527d7ac9
BX <xubill0707@gmail.com>
2023-06-25 22:35:21 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ ticker = "MSFT"
 response = requests.get(f"https://financialmodelingprep.com/api/v3/financial-statement-full-as-reported/{ticker}?apikey={config.api_key}")
 print(response.json())
 
-# manipulate data
+# manipulate data here
