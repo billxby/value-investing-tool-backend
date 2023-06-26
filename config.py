2936fb3f961ee4ddfb2b8c6c446c8bd8161c817d
BX <xubill0707@gmail.com>
2023-06-25 22:29:59 -0400

    Initial Commit

---
+++ config.py
@@ -0,0 +1,1 @@
+api_key = "95cfa265578db4353a576698a3cd6fe2"
---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+import config
+
+ticker = "MSFT"
+
+response = requests.get(f"https://financialmodelingprep.com/api/v3/financial-statement-full-as-reported/{ticker}?apikey={config.api_key}")
+print(response.json())
+
+# manipulate data
