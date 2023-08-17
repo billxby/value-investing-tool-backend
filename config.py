9bda50f91fee48685254997e53f92fb73afa202f
BX <xubill0707@gmail.com>
2023-08-17 11:39:50 -0400

    Finish backend

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 avgRateOfTax = 0.21
+costOfEquity = [0.7, 0.9, 0.12]
+costOfDebt = [0.5, 0.7, 0.9] # the treasury yield is currently 4.3% let us assume companies have 0.5 at low risk etc.
 
-
-
+debtFinancingRatio = .3
--- main.py
+++ main.py
@@ -5,39 +5,56 @@ import os
 
 load_dotenv()
 
-ticker = "MSFT"
+ticker = "WMT"
+riskLevel = 0
+# riskLevel 0, 1, or 2
 api_key = os.getenv("API_KEY")
 
-incomeStatementReq = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement-as-reported/{ticker}?limit=10&apikey={api_key}")
+incomeStatementReq = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit=120&apikey={api_key}")
+balanceSheetReq = requests.get(f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{ticker}?limit=120&apikey={api_key}")
+cashFlowReq = requests.get(f"https://financialmodelingprep.com/api/v3/cash-flow-statement/{ticker}?limit=120&apikey={api_key}")
 
-incomeStatement = incomeStatementReq.json()
+if (incomeStatementReq.status_code != 200 or balanceSheetReq.status_code != 200 or cashFlowReq.status_code != 200):
+    print("Failed to get data")
+    exit()
 
 # manipulate data here
 
-if (incomeStatementReq.status_code != 200):
-    print("Failed to get Income Statements")
-    exit()
-
-
+incomeStatement = incomeStatementReq.json()
+balanceSheet = balanceSheetReq.json()
+cashFlow = cashFlowReq.json()
 
 yearsCount = 0
 avgRevenue = 0
 avgGrossProfit = 0
+avgOperatingMargin = 0
 
 # 5 year average margins return
 # If I have time, fit a model and then see if uptrend and downtrend and then offset avgRevenue and avgGrossProfit
-for i in range(5):
-    if ("revenuefromcontractwithcustomerexcludingassessedtax" in incomeStatement[i]):
-        grossProfit = incomeStatement[i]["revenuefromcontractwithcustomerexcludingassessedtax"] - incomeStatement[i]["costofgoodsandservicessold"]
-        avgRevenue += (incomeStatement[i]["revenuefromcontractwithcustomerexcludingassessedtax"])
-        avgGrossProfit += grossProfit/(incomeStatement[i]["revenuefromcontractwithcustomerexcludingassessedtax"])
+for i in range(len(incomeStatement)):
+    if ("revenue" in incomeStatement[i]):
+        avgOperatingMargin += incomeStatement[i]["netIncome"] / incomeStatement[i]["revenue"]
+        avgGrossProfit += incomeStatement[i]["grossProfit"]/(incomeStatement[i]["revenue"])
+        avgRevenue += incomeStatement[i]["revenue"]
         yearsCount+=1
 
 avgRevenue/=yearsCount
 avgGrossProfit/=yearsCount
+avgOperatingMargin/=yearsCount
 
 print(avgRevenue)
 print(avgGrossProfit)
+print(avgOperatingMargin)
 print("---------------------------")
 
-estimatedEBIT = avgRevenue*avgGrossProfit
+estimatedEBIT = avgRevenue*avgOperatingMargin
+estimatedEBIT *= (1-config.avgRateOfTax)
+estimatedEBITDA = cashFlow[0]["depreciationAndAmortization"] + estimatedEBIT
+
+print(estimatedEBIT)
+print(estimatedEBITDA)
+
+# Calculate Earning Power Value
+
+EPV = estimatedEBITDA / (config.costOfDebt[riskLevel]*config.debtFinancingRatio+config.costOfEquity[riskLevel]*(1-config.debtFinancingRatio))
+print(EPV)
